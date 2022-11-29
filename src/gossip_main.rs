c89b14ef224acecf557d048e915b70485f8c869b
behzad nouri <behzadnouri@gmail.com>
2022-11-29 10:37:53 -0500

    adds warm-up-rounds and gossip-push-capacity

--- src/gossip_main.rs
+++ src/gossip_main.rs
@@ -74,6 +74,13 @@ fn main() {
                 .help("gossip push fanout"),
         )
         .arg(
+            Arg::with_name("gossip_push_capacity")
+                .long("gossip-push-capacity")
+                .takes_value(true)
+                .default_value("5")
+                .help("gossip push fanout"),
+        )
+        .arg(
             Arg::with_name("packet_drop_rate")
                 .long("packet-drop-rate")
                 .takes_value(true)
@@ -94,6 +101,12 @@ fn main() {
                 .default_value("4")
                 .help("number of new CRDS value to generate per gossip round"),
         )
+        .arg(
+            Arg::with_name("warm_up_rounds")
+                .long("warm-up-rounds")
+                .takes_value(true)
+                .help("Number of gossip rounds before collecting stats"),
+        )
         .get_matches();
 
     let json_rpc_url = match matches.value_of("json_rpc_url").unwrap_or_default() {
@@ -103,15 +116,18 @@ fn main() {
     };
     info!("json_rpc_url: {}", json_rpc_url);
     let rpc_client = RpcClient::new(json_rpc_url);
+    let num_crds = matches.value_of_t_or_exit("num_crds");
     let config = Config {
         gossip_push_fanout: matches.value_of_t_or_exit("gossip_push_fanout"),
+        gossip_push_capacity: matches.value_of_t_or_exit("gossip_push_capacity"),
         packet_drop_rate: matches.value_of_t_or_exit("packet_drop_rate"),
-        num_crds: matches.value_of_t_or_exit("num_crds"),
+        num_crds,
         refresh_rate: matches.value_of_t_or_exit("refresh_rate"),
         num_threads: matches
             .value_of_t("num_threads")
             .unwrap_or_else(|_| num_cpus::get()),
         run_duration: Duration::from_secs(matches.value_of_t_or_exit("run_duration")),
+        warm_up_rounds: matches.value_of_t("warm_up_rounds").unwrap_or(2 * num_crds),
     };
     info!("config: {:#?}", config);
     assert!(config.num_threads > 0);
@@ -156,8 +172,8 @@ fn main() {
     // For each node compute how fresh its CRDS table is.
     nodes.sort_unstable_by_key(|node| Reverse(node.stake()));
     let active_stake: u64 = nodes.iter().map(|node| node.stake()).sum();
-    println!("node     | stake | rounds | table | crds");
-    println!("------------------------------------------");
+    println!("node     | stake | rounds |    table | crds");
+    println!("-------------------------------------------");
     for node in &nodes {
         let node_table = node.table();
         let num_hits = table
@@ -165,7 +181,7 @@ fn main() {
             .filter(|(key, ordinal)| node_table.get(key) == Some(ordinal))
             .count();
         println!(
-            "{} | {:.2}% | {:6} | {:5} | {:2}%",
+            "{} | {:.2}% | {:6} | {:8} | {:2}%",
             &format!("{}", node.pubkey())[..8],
             node.stake() as f64 * 100.0 / active_stake as f64,
             node.num_gossip_rounds(),
